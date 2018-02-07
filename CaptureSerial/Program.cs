2866e8b5acb1232154c63c0d241da8ac33b3ff52
NSFW <nsfw@outlook.com>
2018-02-06 22:18:24 -0800

    CaptureSerial RC1

--- CaptureSerial/Program.cs
+++ CaptureSerial/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,102 @@ namespace CaptureSerial
     {
         static void Main(string[] args)
         {
+            try
+            {
+                string portName;
+                int baud;
+                string fileName;
+                if (!Program.TryParseParameters(args, out portName, out baud, out fileName))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Usage instructions:");
+                    Console.WriteLine();
+                    Console.WriteLine("    CaptureSerial <port-name> <baud> [file-name]");
+                    Console.WriteLine();
+                    Console.WriteLine("port-name: name of the serial port, e.g. COM1, COM2, COM3, etc. Required.");
+                    Console.WriteLine("baud: Baud rate, e.g. 9600, 19200, etc. Required.");
+                    Console.WriteLine("file-name: name of the file to write to. Optional.");
+                    Console.WriteLine("If no file name is provided, a name based on the date and time will be used.");
+                    Console.WriteLine("For example, 20171231_235959.txt");
+                    return;
+                }
+
+                Program.Capture(portName, baud, fileName);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.ToString());
+            }
+        }
+
+        private static bool TryParseParameters(string[] args, out string portName, out int baud, out string fileName)
+        {
+            switch (args.Length)
+            {
+                case 2:
+                    portName = args[0];
+
+                    if (!int.TryParse(args[1], out baud))
+                    {
+                        fileName = null;
+                        return false;
+                    }
+
+                    fileName = DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".txt";
+                    return true;
+
+                case 3:
+                    portName = args[0];
+
+                    if (!int.TryParse(args[1], out baud))
+                    {
+                        fileName = null;
+                        return false;
+                    }
+
+                    fileName = args[2];
+                    return true;
+
+                default:
+                    portName = null;
+                    baud = 0;
+                    fileName = null;
+                    return false;
+            }
+        }
+
+        private static void Capture(string portName, int baud, string fileName)
+        {
+            Console.WriteLine("Press any key to stop.");
+            byte[] buffer = new byte[1024 * 64];
+            int totalBytes = 0;
+
+            using (Stream output = File.OpenWrite(fileName))
+            using (SerialPort port = new SerialPort(portName, baud, Parity.None, 8, StopBits.One))
+            {
+                port.DataReceived +=
+                    async delegate (object sender, SerialDataReceivedEventArgs e)
+                    {
+                        int bytes = Math.Min(port.BytesToRead, buffer.Length);
+                        totalBytes += bytes;
+                        port.Read(buffer, 0, bytes);
+                        await output.WriteAsync(buffer, 0, bytes);
+                    };
+
+                port.Open();
+
+                while (!Console.KeyAvailable)
+                {
+                    System.Threading.Thread.Sleep(100);
+                    Console.CursorLeft = 0;
+                    Console.Write(string.Format("{0} bytes captured.  ", totalBytes));
+                }
+
+                port.Close();
+
+                // Let the last buffer flush.
+                System.Threading.Thread.Sleep(500);
+            }
         }
     }
 }
