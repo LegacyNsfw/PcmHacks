97bb186d4390ff9a445296d396aadd77b0c9b868
antuspcm <ant@symons.net.au>
2018-03-14 08:21:08 +1030

    fix block comments

--- Prototype/Flash411/Messages/BlockId.cs
+++ Prototype/Flash411/Messages/BlockId.cs
@@ -8,14 +8,14 @@ namespace Flash411
 {
     class BlockId
     {
-        public const byte Vin1               = 0x01;
-        public const byte Vin2               = 0x02;
-        public const byte Vin3               = 0x03;
-        public const byte Serial1            = 0x04;
-        public const byte Serial2            = 0x05;
-        public const byte Serial3            = 0x06;
-        public const byte Serial4            = 0x07;
-        public const byte CalibrationID      = 0x08;
+        public const byte Vin1               = 0x01; // 5 bytes of VIN
+        public const byte Vin2               = 0x02; // 6 bytes of VIN
+        public const byte Vin3               = 0x03; // 6 bytes of VIN
+        public const byte Serial1            = 0x04; // 4 bytes of Serial
+        public const byte Serial2            = 0x05; // 4 bytes of Serial
+        public const byte Serial3            = 0x06; // 4 bytes of Serial
+        public const byte HardwareID         = 0x07; // Hardware ID
+        public const byte CalibrationID      = 0x08; // Calibration ID
         public const byte OSID               = 0x0A; // Operating System ID
         public const byte EngineCalID        = 0x0B; // Engine Segment Calibration ID
         public const byte EngineDiagCalID    = 0x0C; // Engine Diagnostic Calibration ID
