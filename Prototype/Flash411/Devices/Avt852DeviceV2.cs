f80f358d318f285b06bc1c6665ec5a79563a02d4
antuspcm <ant@symons.net.au>
2018-03-17 01:16:08 +1030

    contains a complete avt packet reader, but does not work overall

--- Prototype/Flash411/Devices/Avt852DeviceV2.cs
+++ Prototype/Flash411/Devices/Avt852DeviceV2.cs
@@ -15,16 +15,16 @@ namespace Flash411
     /// 
     class Avt852DeviceV2 : Device
     {
-        public static readonly byte[] AVT_RESET = { 0xF1, 0xA5 };
-        public static readonly byte[] AVT_ENTER_VPW_MODE = { 0xE1, 0x33 };
-        public static readonly byte[] AVT_REQUEST_MODEL = { 0xF0 };
-        public static readonly byte[] AVT_REQUEST_FIRMWARE = { 0xB0 };
+        public static readonly Message AVT_RESET = new Message(new byte[] { 0xF1, 0xA5 });
+        public static readonly Message AVT_ENTER_VPW_MODE = new Message(new byte[] { 0xE1, 0x33 });
+        public static readonly Message AVT_REQUEST_MODEL = new Message(new byte[] { 0xF0 });
+        public static readonly Message AVT_REQUEST_FIRMWARE = new Message(new byte[] { 0xB0 });
 
-        public static readonly byte[] AVT_VPW = { 0x91, 0x07 };
-        public static readonly byte[] AVT_852_IDLE = { 0x91, 0x27 };
-        public static readonly byte[] AVT_842_IDLE = { 0x91, 0x12 };
-        public static readonly byte[] AVT_TX_ACK = { 0x01, 0x60 };
-        public static readonly byte[] AVT_BLOCK_TX_ACK = { 0xF3, 0x60 };
+        public static readonly Message AVT_VPW = new Message(new byte[] { 0x07 }); // 91 07
+        public static readonly Message AVT_852_IDLE = new Message(new byte[] { 0x27 }); // 91 27
+        public static readonly Message AVT_842_IDLE = new Message(new byte[] { 0x12 }); // 92 12
+        public static readonly Message AVT_TX_ACK = new Message(new byte[] { 0x60 }); // 01 60
+        public static readonly Message AVT_BLOCK_TX_ACK = new Message(new byte[] { 0x60 }); // F3 60 XX XX
 
         public Avt852DeviceV2(IPort port, ILogger logger) : base(port, logger)
         {
@@ -45,7 +45,8 @@ namespace Flash411
         /// </summary>
         private async void DataReceived(object source, SerialDataReceivedEventArgs args)
         {
-            while(await this.Port.GetReceiveQueueSize() > 0)
+            this.Logger.AddDebugMessage("Trace: DataReceived");
+            while (await this.Port.GetReceiveQueueSize() > 0)
             {
                 byte[] buffer = new byte[await this.Port.GetReceiveQueueSize()];
                 int length = await this.Port.Receive(buffer, 0, buffer.Length);
@@ -77,21 +78,21 @@ namespace Flash411
             SerialPortConfiguration configuration = new SerialPortConfiguration();
             configuration.BaudRate = 115200;
             configuration.DataReceived = this.DataReceived;
+            byte[] response= { 0 };
 
             await this.Port.OpenAsync(configuration);
 
-            await this.Port.Send(Avt852DeviceV2.AVT_RESET);
+            await SendRequest(Avt852DeviceV2.AVT_RESET);
 
-            AvtMessage response = await this.GetResponse();
             if (response == null)
             {
                 this.Logger.AddUserMessage("AVT device not found or failed reset. No response.");
                 return false;
             }
-
+            /*
             byte[] reply = response.Data;
 
-            if (Utility.CompareArrays(reply, Avt852DeviceV2.AVT_852_IDLE))
+            if (Utility.CompareArrays(reply, Avt852DeviceV2.AVT_852_IDLE.GetBytes()))
             {
                 this.Logger.AddUserMessage("AVT device reset ok");
             }
@@ -102,6 +103,8 @@ namespace Flash411
                 this.Logger.AddDebugMessage("Received " + reply);
                 return false;
             }
+
+            await Task.Delay(100);
 
             await this.Port.Send(Avt852DeviceV2.AVT_ENTER_VPW_MODE);
 
@@ -114,7 +117,7 @@ namespace Flash411
 
             byte[] reply2 = response.Data;
 
-            if (Utility.CompareArrays(reply2, Avt852DeviceV2.AVT_VPW))
+            if (Utility.CompareArrays(reply2, Avt852DeviceV2.AVT_VPW.GetBytes()))
             {
                 this.Logger.AddUserMessage("AVT device to VPW mode");
 
@@ -126,12 +129,13 @@ namespace Flash411
                 this.Logger.AddDebugMessage("Received " + reply2);
                 return false;
             }
-
+            */
             return true;
         }
 
         private async Task<AvtMessage> GetResponse()
         {
+            this.Logger.AddDebugMessage("Trace: GetResponse");
             AvtMessage response = null;
             for (int iteration = 0; iteration < 10; iteration++)
             {
@@ -151,6 +155,7 @@ namespace Flash411
         /// </summary>
         public override Task<bool> SendMessage(Message message)
         {
+            this.Logger.AddDebugMessage("Trace: SendMessage");
             StringBuilder builder = new StringBuilder();
             this.Logger.AddDebugMessage("Sending message " + message.GetBytes().ToHex());
             this.Port.Send(message.GetBytes());
@@ -162,19 +167,67 @@ namespace Flash411
         /// </summary>
         public override Task<Response<byte[]>> SendRequest(Message message)
         {
-
+            this.Logger.AddDebugMessage("Trace: SendRequest");
+            int length = 0;
+            bool status = true; // do we have a status byte? (we dont for some 9x init commands)
             StringBuilder builder = new StringBuilder();
             this.Logger.AddDebugMessage("TX: " + message.GetBytes().ToHex());
             this.Port.Send(message.GetBytes());
+            byte[] rx = new byte[2];
 
-            // This code here will need to handle AVT packet formatting
-            byte[] response = new byte[100];
-            this.Port.Receive(response, 0, 2);
+            // Get the first packet byte.
+            this.Port.Receive(rx, 0, 1);
 
-            this.Logger.AddDebugMessage("RX: " + message.GetBytes().ToHex());
-            this.Port.Send(message.GetBytes());
+            // read an AVT format length
+            switch (rx[0])
+            {
+                case 0x11:
+                    this.Port.Receive(rx, 0, 1);
+                    length = rx[0];
+                    break;
+                case 0x12:
+                    this.Port.Receive(rx, 0, 1);
+                    length = rx[0] << 8;
+                    this.Port.Receive(rx, 0, 1);
+                    length += rx[0];
+                    break;
+                case 0x23:
+                    this.Port.Receive(rx, 0, 1);
+                    if (rx[0] != 0x53)
+                    {
+                        this.Logger.AddDebugMessage("RX: VPW too long: " + rx[0].ToString("X2"));
+                        return Task.FromResult(Response.Create(ResponseStatus.Error, rx));
+                    }
+                    this.Port.Receive(rx, 0, 2);
+                    this.Logger.AddDebugMessage("RX: VPW too long and truncated to " + ((rx[0] << 8) + rx[1]).ToString("X4"));
+                    length = 4112;
+                    break;
+                default:
+                    this.Logger.AddDebugMessage("default status: " + rx[0].ToString("X2"));
+                    length = rx[0] & 0x0F;
+                    if ((rx[0] & 0xF0) == 0x90) // special case, init packet with no status
+                    {
+                        this.Logger.AddDebugMessage("Dont read status, 9X");
+                        status = false;
+                    }
+                    
+                    break;
+            }
 
-            return Task.FromResult(Response.Create(ResponseStatus.Success, response));
+            // if we need to get check and discard the status byte
+            if (status == true)
+            {
+                this.Port.Receive(rx, 0, 1);
+                if (rx[0] != 0) this.Logger.AddDebugMessage("RX: bad packet status: " + rx[0].ToString("X2"));
+            }
+
+            // return the packet
+            byte[] receive = new byte[length];
+            Task.Delay(500);
+            this.Port.Receive(receive, 0, length);
+            this.Logger.AddDebugMessage("Length=" + length + " RX: " + receive.ToHex());
+
+            return Task.FromResult(Response.Create(ResponseStatus.Success, receive));
         }
     }
 }
