9c3d8962f012771d2b0123fbfcd8f188dfa4430b
antuspcm <ant@symons.net.au>
2018-06-14 19:46:11 +0930

    0411_256px.ico

--- Prototype/Flash411/Properties/AssemblyInfo.cs
+++ Prototype/Flash411/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Flash411")]
+[assembly: AssemblyProduct("PCM Hammer")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
