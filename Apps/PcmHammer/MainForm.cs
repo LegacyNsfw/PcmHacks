98d942e160d1d1e5ac813e64190e954601e7eb4b
NSFW <nsfw@outlook.com>
2018-08-14 21:15:49 -0700

    Undid a local-testing hack for the help content.

--- Apps/PcmHammer/MainForm.cs
+++ Apps/PcmHammer/MainForm.cs
@@ -119,7 +119,7 @@ namespace PcmHacking
                     HttpClient client = new HttpClient();
                     var response = await client.SendAsync(request);
 
-                    if (response.StatusCode == HttpStatusCode.NotFound)
+                    if (response.StatusCode == HttpStatusCode.OK)
                     {
                         this.helpWebBrowser.DocumentStream = await response.Content.ReadAsStreamAsync();
                     }
