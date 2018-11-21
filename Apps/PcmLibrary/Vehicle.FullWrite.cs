28b3ac91ad0abb8e1329923a521df9cef89f4fe0
NSFW <nsfw@outlook.com>
2018-11-21 00:12:37 -0800

    Split the Vehicle class into multiple files.

---
+++ Apps/PcmLibrary/Vehicle.FullWrite.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PcmHacking
+{
+    public partial class Vehicle
+    {
+        /// <summary>
+        /// Replace the full contents of the PCM.
+        /// </summary>
+        public Task<bool> WriteContents(Stream stream)
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
---
+++ Apps/PcmLibrary/Vehicle.Properties.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PcmHacking
+{
+    /// <summary>
+    /// From the application's perspective, this class is the API to the vehicle.
+    /// </summary>
+    /// <remarks>
+    /// Methods in this class are high-level operations like "get the VIN," or "read the contents of the EEPROM."
+    /// </remarks>
+    public partial class Vehicle : IDisposable
+    {
+        /// <summary>
+        /// Query the PCM's VIN.
+        /// </summary>
+        public async Task<Response<string>> QueryVin()
+        {
+            await this.device.SetTimeout(TimeoutScenario.ReadProperty);
+
+            this.device.ClearMessageQueue();
+
+            if (!await this.device.SendMessage(this.messageFactory.CreateVinRequest1()))
+            {
+                return Response.Create(ResponseStatus.Timeout, "Unknown. Request for block 1 failed.");
+            }
+
+            Message response1 = await this.device.ReceiveMessage();
+            if (response1 == null)
+            {
+                return Response.Create(ResponseStatus.Timeout, "Unknown. No response to request for block 1.");
+            }
+
+            if (!await this.device.SendMessage(this.messageFactory.CreateVinRequest2()))
+            {
+                return Response.Create(ResponseStatus.Timeout, "Unknown. Request for block 2 failed.");
+            }
+
+            Message response2 = await this.device.ReceiveMessage();
+            if (response2 == null)
+            {
+                return Response.Create(ResponseStatus.Timeout, "Unknown. No response to request for block 2.");
+            }
+
+            if (!await this.device.SendMessage(this.messageFactory.CreateVinRequest3()))
+            {
+                return Response.Create(ResponseStatus.Timeout, "Unknown. Request for block 3 failed.");
+            }
+
+            Message response3 = await this.device.ReceiveMessage();
+            if (response3 == null)
+            {
+                return Response.Create(ResponseStatus.Timeout, "Unknown. No response to request for block 3.");
+            }
+
+            return this.messageParser.ParseVinResponses(response1.GetBytes(), response2.GetBytes(), response3.GetBytes());
+        }
+
+        /// <summary>
+        /// Query the PCM's Serial Number.
+        /// </summary>
+        public async Task<Response<string>> QuerySerial()
+        {
+            await this.device.SetTimeout(TimeoutScenario.ReadProperty);
+
+            this.device.ClearMessageQueue();
+
+            if (!await this.device.SendMessage(this.messageFactory.CreateSerialRequest1()))
+            {
+                return Response.Create(ResponseStatus.Timeout, "Unknown. Request for block 1 failed.");
+            }
+
+            Message response1 = await this.device.ReceiveMessage();
+            if (response1 == null)
+            {
+                return Response.Create(ResponseStatus.Timeout, "Unknown. No response to request for block 1.");
+            }
+
+            if (!await this.device.SendMessage(this.messageFactory.CreateSerialRequest2()))
+            {
+                return Response.Create(ResponseStatus.Timeout, "Unknown. Request for block 2 failed.");
+            }
+
+            Message response2 = await this.device.ReceiveMessage();
+            if (response2 == null)
+            {
+                return Response.Create(ResponseStatus.Timeout, "Unknown. No response to request for block 2.");
+            }
+
+            if (!await this.device.SendMessage(this.messageFactory.CreateSerialRequest3()))
+            {
+                return Response.Create(ResponseStatus.Timeout, "Unknown. Request for block 3 failed.");
+            }
+
+            Message response3 = await this.device.ReceiveMessage();
+            if (response3 == null)
+            {
+                return Response.Create(ResponseStatus.Timeout, "Unknown. No response to request for block 3.");
+            }
+
+            return this.messageParser.ParseSerialResponses(response1, response2, response3);
+        }
+
+        /// <summary>
+        /// Query the PCM's Broad Cast Code.
+        /// </summary>
+        public async Task<Response<string>> QueryBCC()
+        {
+            await this.device.SetTimeout(TimeoutScenario.ReadProperty);
+
+            var query = this.CreateQuery(
+                this.messageFactory.CreateBCCRequest,
+                this.messageParser.ParseBCCresponse);
+
+            return await query.Execute();
+        }
+
+        /// <summary>
+        /// Query the PCM's Manufacturer Enable Counter (MEC)
+        /// </summary>
+        public async Task<Response<string>> QueryMEC()
+        {
+            await this.device.SetTimeout(TimeoutScenario.ReadProperty);
+
+            var query = this.CreateQuery(
+                this.messageFactory.CreateMECRequest,
+                this.messageParser.ParseMECresponse);
+
+            return await query.Execute();
+        }
+
+        /// <summary>
+        /// Update the PCM's VIN
+        /// </summary>
+        /// <remarks>
+        /// Requires that the PCM is already unlocked
+        /// </remarks>
+        public async Task<Response<bool>> UpdateVin(string vin)
+        {
+            this.device.ClearMessageQueue();
+
+            if (vin.Length != 17) // should never happen, but....
+            {
+                this.logger.AddUserMessage("VIN " + vin + " is not 17 characters long!");
+                return Response.Create(ResponseStatus.Error, false);
+            }
+
+            this.logger.AddUserMessage("Changing VIN to " + vin);
+
+            byte[] bvin = Encoding.ASCII.GetBytes(vin);
+            byte[] vin1 = new byte[6] { 0x00, bvin[0], bvin[1], bvin[2], bvin[3], bvin[4] };
+            byte[] vin2 = new byte[6] { bvin[5], bvin[6], bvin[7], bvin[8], bvin[9], bvin[10] };
+            byte[] vin3 = new byte[6] { bvin[11], bvin[12], bvin[13], bvin[14], bvin[15], bvin[16] };
+
+            this.logger.AddUserMessage("Block 1");
+            Response<bool> block1 = await WriteBlock(BlockId.Vin1, vin1);
+            if (block1.Status != ResponseStatus.Success) return Response.Create(ResponseStatus.Error, false);
+            this.logger.AddUserMessage("Block 2");
+            Response<bool> block2 = await WriteBlock(BlockId.Vin2, vin2);
+            if (block2.Status != ResponseStatus.Success) return Response.Create(ResponseStatus.Error, false);
+            this.logger.AddUserMessage("Block 3");
+            Response<bool> block3 = await WriteBlock(BlockId.Vin3, vin3);
+            if (block3.Status != ResponseStatus.Success) return Response.Create(ResponseStatus.Error, false);
+
+            return Response.Create(ResponseStatus.Success, true);
+        }
+
+        /// <summary>
+        /// Query the PCM's operating system ID.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<Response<UInt32>> QueryOperatingSystemId()
+        {
+            return await this.QueryUnsignedValue(this.messageFactory.CreateOperatingSystemIdReadRequest);
+        }
+
+        /// <summary>
+        /// Query the PCM's Hardware ID.
+        /// </summary>
+        /// <remarks>
+        /// Note that this is a software variable and my not match the hardware at all of the software runs.
+        /// </remarks>
+        /// <returns></returns>
+        public async Task<Response<UInt32>> QueryHardwareId()
+        {
+            return await this.QueryUnsignedValue(this.messageFactory.CreateHardwareIdReadRequest);
+        }
+
+        /// <summary>
+        /// Query the PCM's Hardware ID.
+        /// </summary>
+        /// <remarks>
+        /// Note that this is a software variable and my not match the hardware at all of the software runs.
+        /// </remarks>
+        /// <returns></returns>
+        public async Task<Response<UInt32>> QueryCalibrationId()
+        {
+            await this.device.SetTimeout(TimeoutScenario.ReadProperty);
+
+            var query = this.CreateQuery(
+                this.messageFactory.CreateCalibrationIdReadRequest,
+                this.messageParser.ParseBlockUInt32);
+            return await query.Execute();
+        }
+
+        /// <summary>
+        /// Helper function to create Query objects.
+        /// </summary>
+        private Query<T> CreateQuery<T>(Func<Message> generator, Func<Message, Response<T>> filter)
+        {
+            return new Query<T>(this.device, generator, filter, this.logger);
+        }
+
+        /// <summary>
+        /// Helper function for queries that return unsigned 32-bit integers.
+        /// </summary>
+        private async Task<Response<UInt32>> QueryUnsignedValue(Func<Message> generator)
+        {
+            await this.device.SetTimeout(TimeoutScenario.ReadProperty);
+
+            var query = this.CreateQuery(generator, this.messageParser.ParseBlockUInt32);
+            return await query.Execute();
+        }
+    }
+}
