e1d94712a438963149abe2137ae52aa37ab56f92
antuspcm <ant@symons.net.au>
2018-12-04 20:29:27 +1030

    use gnu tools for srec to bin. pause at end of batch execution

--- Kernels/build.bat
+++ Kernels/build.bat
@@ -1,4 +1,4 @@
-@echo off
+#@echo off
 : -fomit-frame-pointer = remove the boilerplate linkw/unlk instructions 
 : -c    = compile but do not link
 : -O1   = optimization level
@@ -9,18 +9,14 @@ c:\SysGCC\m68k-elf\bin\m68k-elf-gcc.exe -c -fomit-frame-pointer -std=gnu99 -mcpu
 c:\SysGCC\m68k-elf\bin\m68k-elf-ld.exe -T micro-kernel.ld main.o micro-kernel.o
 c:\SysGCC\m68k-elf\bin\m68k-elf-objcopy.exe -O srec a.out micro-kernel.S
 C:\SysGCC\m68k-elf\bin\m68k-elf-objdump.exe -d -S a.out > micro-kernel.disassembly
+C:\SysGCC\m68k-elf\bin\m68k-elf-objcopy.exe --input-target=srec --output-target=binary micro-kernel.S micro-kernel.bin
 
 c:\SysGCC\m68k-elf\bin\m68k-elf-gcc.exe -c -fomit-frame-pointer -std=gnu99 -mcpu=68332 -O0 main.c common.c read-kernel.c 
 c:\SysGCC\m68k-elf\bin\m68k-elf-ld.exe -T read-kernel.ld main.o common.o read-kernel.o
 c:\SysGCC\m68k-elf\bin\m68k-elf-objcopy.exe -O srec a.out read-kernel.S
 C:\SysGCC\m68k-elf\bin\m68k-elf-objdump.exe -d -S a.out > read-kernel.disassembly
-
-del micro-kernel.bin
-..\DevTools\SRecordToKernel\bin\Debug\netcoreapp2.1\win10-x64\SRecordToKernel.exe micro-kernel.S micro-kernel.bin > micro-kernel.log
-dir micro-kernel.bin | findstr File
-
-del read-kernel.bin
-..\DevTools\SRecordToKernel\bin\Debug\netcoreapp2.1\win10-x64\SRecordToKernel.exe read-kernel.S read-kernel.bin > read-kernel.log
-dir read-kernel.bin | findstr File
+C:\SysGCC\m68k-elf\bin\m68k-elf-objcopy.exe --input-target=srec --output-target=binary read-kernel.S read-kernel.bin
 
 copy *-kernel.bin ..\Apps\PcmHammer\bin\debug
+
+pause
