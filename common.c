5cc6e55e9379f9b63ba2a9a5b912a709ecb7d2da
NSFW <nsfw@outlook.com>
2019-01-20 23:19:29 -0800

    Version bump.

--- common.c
+++ common.c
@@ -461,7 +461,7 @@ void HandleVersionQuery(uint8_t kernelType)
 	MessageBuffer[3] = 0x7D;
 	MessageBuffer[4] = 0x00;
 	MessageBuffer[5] = 0x01; // major
-	MessageBuffer[6] = 0x01; // minor
+	MessageBuffer[6] = 0x02; // minor
 	MessageBuffer[7] = 0x00; // patch
 	MessageBuffer[8] = kernelType; // AA = read, BB = write
 
