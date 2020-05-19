50d892f51d327c7ff3e28ac3a83814e3c5c589f6
GampyG28 <gampyg28@gmail.com>
2020-05-19 03:04:14 -0400

    Build kernel with options, most specifically the kernel base address.

--- Kernels/clean.bat
+++ Kernels/clean.bat
@@ -1,9 +1,13 @@
-del *.o
-del *.disassembly
-del *.out
-del *.S
-del *.bin
-del *.elf
-del *.log
-del *.exe
+@echo off
+for %%A in (
+  *.o
+  *.disassembly
+  *.out
+  *.S
+  *.bin
+  *.elf
+  *.log
+  *.exe
+  *.map
+  ) do if exist %%A echo   Deleting %%A & del %%A
 
