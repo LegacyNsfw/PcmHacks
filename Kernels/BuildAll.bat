a43b3b25046ebbed009968bcba6050f3782c5a5a
antus <antus@pcmhacking.net>
2022-04-10 21:16:56 +0930

    whitespace

--- Kernels/BuildAll.bat
+++
@@ -1,5 +0,0 @@
-del *.bin
-call .\Build.cmd 
-call .\Build.cmd -aFFB800 -tP10
-call .\Build.cmd -aFF2000 -tP12
-dir *.bin
--- Kernels/build.bat
+++ Kernels/build.bat
@@ -14,6 +14,7 @@ rem * Build.cmd defaults to P01 (and P59).
 rem * Example P01: Build.cmd
 rem * Example P10: Build.cmd -aFFB800 -tP10
 rem * Example P12: Build.cmd -aFF2000 -tP12
+
 call Build.cmd -aFF8000 -tP01 -m -d
 call Build.cmd -aFFB800 -tP10
 call Build.cmd -aFF2000 -tP12
