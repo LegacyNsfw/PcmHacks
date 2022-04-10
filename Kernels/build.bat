c446a8e0652c162aad880300bd9adfa534dad8e1
antus <antus@pcmhacking.net>
2022-04-10 18:05:53 +0930

    display hardware type. skip hw type from pcm (not used, and not necessarily correct for the hw)

--- Kernels/build.bat
+++ Kernels/build.bat
@@ -7,16 +7,19 @@
 
 if -%KernelBuild%- == -- call fixpath.bat
 
-call clean.bat
+call clean.cmd
 
 rem * Please See: Build.cmd -h
 rem * Build.cmd defaults to P01 (and P59).
 rem * Example P01: Build.cmd
 rem * Example P10: Build.cmd -aFFB800 -tP10
 rem * Example P12: Build.cmd -aFF2000 -tP12
-call Build.cmd -m -d
+call Build.cmd -aFF8000 -tP01 -m -d
+call Build.cmd -aFFB800 -tP10
+call Build.cmd -aFF2000 -tP12
 
 c:\mingw\bin\g++ -o test.exe test.cpp crc.c
 
 dir *.bin
+pause
 
